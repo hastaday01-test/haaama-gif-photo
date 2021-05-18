369cbe532c89ef80172cf5f7da794ed95c14e51b
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-18 16:58:05 +0300

    Update help.js

--- commandgif/help.js
+++ commandgif/help.js
@@ -24,8 +24,7 @@ const embed = new Discord.MessageEmbed()
 \`emoji\`
 
 **Text Commands**
-** \`text\` **
-\`tEng\` - \`tArab\` - \`tKurd\` 
+\`${PREFIX}tEng\` - \`tArab\` - \`tKurd\` 
 
 **Links**
 **[Invite](https://discord.com/api/oauth2/authorize?client_id=814607957349629962&permissions=8&scope=bot) - [Server Support](https://discord.gg/jy6zvqV2V6)** **-** [Vote](https://top.gg/bot/814607957349629962)`)

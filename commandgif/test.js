26a6bfc4f4573857643284a374bf70023443351f
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-22 16:41:45 +0300

    Create test.js

---
+++ commandgif/test.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const moment = require('moment');
+var config = require('../config.json');
+
+exports.run = async (client, message, args, msg) => {
+
+const embed = new Discord.MessageEmbed()
+    
+.setColor('#d60000') 
+.setThumbnail(message.author.avatarURL({dynamic: "true"}))
+// .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+.setImage(`https://media.discordapp.net/attachments/790836700561670145/845056457162489876/image0.gif`)
+.setAuthor(`Gif Help`)
+.setDescription(`
+**Info Commands** (*)
+\`server\` - \`avatar\` - \`about\` 
+\`ping\` - \`inv\` - \`clear\`
+\`say\`
+
+**Gifs Commands**
+\`boy\` - \`girl\` - \`baby\`
+\`neon\` - \`sad\` - \`smoking\`
+\`animal\` - \`anime\` - \`couple\`
+\`emoji\`
+
+**Text Commands**
+\`tEng\` - \`tArab\` - \`tKurd\`,
+
+**Links**
+**[Invite](https://discord.com/api/oauth2/authorize?client_id=814607957349629962&permissions=8&scope=bot) - [Server Support](https://discord.gg/jy6zvqV2V6)** **-** [Vote](https://top.gg/bot/814607957349629962)`)
+    
+message.channel.send({embed});
+}
+
+exports.conf = {
+enabled: true,
+guildOnly: false,
+aliases: ['help'],
+permLevel: 0
+};
+
+exports.help = {
+  name: 'test',
+  description: 'rexuss',
+  usage: 't'
+};

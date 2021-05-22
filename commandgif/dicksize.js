816a82a93724c3550eed6bdf1891bdf1223bab4b
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-05-22 16:18:38 +0300

    Create dicksize.js

---
+++ commandgif/dicksize.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "text",
+  aliases: ["text", "pp", "ppsize"],
+  description: "Show Member PP Size!",
+  usage: "Dicksize <Mention Member>",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+    let sizes = [
+      "kurd",
+      "roshek",
+      "HaaMa",
+      "juana",
+      "hastaday",
+      "bot dalem law bandana",
+      "dayday",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8D",
+      "D",
+      "hama",
+      "esta to lagal ke D",
+      "poli6",
+      "wa arOm chetr nambenawa",
+      "Rozhek darom",
+      "Dndbs",
+      "hdhwhd"
+    ];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(``)
+      .setDescription(`**\n${Result}**`)
+   // .setFooter(`Requested by ${message.author.username}`)
+  //    .setTimestamp();
+    message.channel.send(embed);
+
+    //End
+  }
+};

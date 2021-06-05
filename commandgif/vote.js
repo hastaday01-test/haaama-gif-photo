3371730a239be8185fd52b1a2083b35eca208a2d
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-05 09:57:47 +0300

    Create vote.js

---
+++ commandgif/vote.js
@@ -0,0 +1,33 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const moment = require('moment');
+var config = require('../config.json');
+
+exports.run = async (client, message, args, msg) => {
+
+const embed = new Discord.MessageEmbed()
+    
+.setColor('#ef0000') 
+.setThumbnail(message.author.avatarURL({dynamic: "true"}))
+// .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+.setImage(`https://media.discordapp.net/attachments/790836700561670145/838461806137442365/image0.gif`)
+.setAuthor(`Vote`)
+.setDescription(`
+[Vote](https://top.gg/bot/814607957349629962)`)
+    
+message.channel.send({embed});
+}
+
+exports.conf = {
+enabled: true,
+guildOnly: false,
+aliases: ['vote'],
+permLevel: 0
+};
+
+exports.help = {
+  name: 'vote',
+  description: 'rexuss',
+  usage: 'v'
+};

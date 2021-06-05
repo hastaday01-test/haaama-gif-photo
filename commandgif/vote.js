2614f341d240eb6ed17def342b5e53f5754e9079
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-05 10:00:23 +0300

    Update vote.js

--- commandgif/vote.js
+++ commandgif/vote.js
@@ -12,6 +12,7 @@ const embed = new Discord.MessageEmbed()
 .setThumbnail(message.author.avatarURL({dynamic: "true"}))
 // .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
 .setImage(`https://media.discordapp.net/attachments/790836700561670145/838461806137442365/image0.gif`)
+.setTitle(`link - https://top.gg/bot/814607957349629962`)
 .setAuthor(`Vote RaGif`)
 .setDescription(`
 [Click Here to Vote](https://top.gg/bot/814607957349629962)`)
